7960481e5dacaba7040c18f00b2aa65c2941f8f3
keilertrindade <keiler.trindade@hotmail.com>
2022-12-15 15:37:56 -0300

    finalização exercicio js

--- Atividades-Aulas/conexao-html-js/js/script.js
+++ Atividades-Aulas/conexao-html-js/js/script.js
@@ -3,17 +3,32 @@ let inputNome = document.getElementById('nome')
 inputNome.value = nome;
 inputNome.disabled = true;
 
+let campoData = document.querySelector('#data')
+let date = new Date()
+campoData.value = date.getFullYear().toString() + '-' + (date.getMonth() + 1).toString() +
+'-' + date.getDate().toString();
+campoData.disabled = true;
+
+
 let comentario = 'Deixe seu comentário aqui '+ inputNome.value+'!';
 console.log(comentario)
 
 const deixeComentario = document.getElementById('deixe-comentario');
 deixeComentario.innerText = comentario;
 
-let usuario = {
+
+
+
+
+
+
+
+
+/*let usuario = {
     id: 5,
     username: "João",
     senha: "abc123",
     admin: true
 }
+console.log(usuario.id)*/
 
-console.log(usuario.id)
